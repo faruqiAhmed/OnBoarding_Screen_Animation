569341878dbe528e78a402547ce2e3f18f079ef0
Faruq <faruqdeveloper@gmail.com>
2021-09-03 20:48:28 +0600

    Initial Commit

---
+++ OnBoarding_Screen_Animation/OnBoarding_Screen_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoarding_Screen_AnimationApp.swift
+//  OnBoarding_Screen_Animation
+//
+//  Created by Md Omar Faruq on 9/3/21.
+//
+
+import SwiftUI
+
+@main
+struct OnBoarding_Screen_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
